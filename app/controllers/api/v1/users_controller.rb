4fcf712520b48c9488c75cf9d47f65686d00abcb
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-02-23 13:05:18 -0600

    Created UsersController to handle new user creation. Params still need to be added to user_params method once users table has been created.

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController 
+  skip_before_action :require_login, only: [:create]
+
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save 
+      payload = {user_id: user.id}
+      token = encode_token(payload)
+      render json: {user: user, jwt: token}
+    else
+      render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params 
+    params.require(:user).permit() # need to add attributes once users table is created
+  end
+
+end
